7c6a01ffd22e0863df5f0966af3a3789eab0fdb4
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2020-07-06 20:23:36 -0300

    Resultados Funcionales

---
+++ Tp Resulto/GraficosClase12/4_mov_triangulo/main.cpp
@@ -0,0 +1,104 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+
+    in vec2 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+
+    out vec4 outColor;
+
+    void main()
+    {
+        outColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+)glsl";
+
+int main(int argc, char* argv[])
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	GLfloat vertices[] =
+	{
+		0.0f,  0.5f,
+		0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// SE AGREGA ESTO PARA MOVERLO. ATENCION: MUY IMPORTANTE...
+		// ESTA FORMA NO ES LA CORRECTA Y ES POCO PERFORMANTE, SOLO SE HACE PARA UN EJEMPLO
+		// DE COMO SE PODIA IMPLEMENTAR MIRANDO CON EL CODIGO DE BASE SIN AGREGAR NADA MAS
+		for (int i = 0; i < 6; i++)
+		{
+			vertices[i] += 0.001;
+
+			GLuint vbo;
+			glGenBuffers(1, &vbo);
+			glBindBuffer(GL_ARRAY_BUFFER, vbo);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+			GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+			glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(posAttrib);
+		}
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glDeleteBuffers(1, &vbo);
+}
---
+++ Tp Resulto/GraficosClase12/6_tecla/main.cpp
@@ -0,0 +1,93 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+
+    in vec2 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+
+    out vec4 outColor;
+
+    void main()
+    {
+        outColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+)glsl";
+
+int main(int argc, char* argv[])
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	GLfloat vertices[] =
+	{
+		0.0f,  0.5f,
+		0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
+
+	float color = 0.0f; // SE AGREGA ESTA VARIABLE
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+		// SE AGREGA ESTE CODIGO PARA DETECTAR SI SE MANTIENE PRESIONADO 1
+		if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+			color = 1.0f;
+		else
+			color = 0.0f;
+
+		glfwPollEvents();
+
+		glClearColor(color, color, color, 1.0f); // SE MODIFICA PARA USAR VARIABLE
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glDeleteBuffers(1, &vbo);
+}
